1ad6ffc8cffa722c72f67ec0a3c13f78cc620bbf
Johannes Kettmann <jkettmann@gmx.net>
2020-07-14 10:42:26 +0200

    Adds routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <GlobalStyle />
+        <Switch>
+          <Route path="/search">Search</Route>
+          <Route path="/">Home</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
