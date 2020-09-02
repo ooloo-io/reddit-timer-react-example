d8304f249e2c440185a87c6be4eba8c944ac46d3
Johannes Kettmann <jkettmann@gmx.net>
2020-09-02 08:12:18 +0200

    Write tests for heatmap

--- src/__tests__/SearchPage.js
+++ src/__tests__/SearchPage.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 import App from '../app';
 import { defaultSubreddit } from '../config';
 
-const setup = (initialPath = '/') => {
+const setup = (initialPath) => {
   // access history as described in the docs
   // https://reactrouter.com/web/guides/testing/checking-location-in-tests
   let history;
@@ -24,43 +24,68 @@ const setup = (initialPath = '/') => {
   return { history };
 };
 
-test('loads top posts for subreddit in URL', async () => {
-  setup('/search/reactjs');
+describe('subreddit form', () => {
+  test('submitting the form updates the URL', () => {
+    const { history } = setup('/search/reactjs');
 
-  screen.getByText('loading-spinner.svg');
+    const searchInput = screen.getByLabelText('r /');
+    expect(searchInput.value).toBe('reactjs');
 
-  expect(await screen.findByText('500')).toBeInTheDocument();
-  expect(screen.queryByText('loading-spinner.svg')).not.toBeInTheDocument();
+    userEvent.clear(searchInput);
+    userEvent.type(searchInput, 'vuejs');
+    expect(searchInput.value).toBe('vuejs');
+
+    const submitButton = screen.getByRole('button', { name: 'Search' });
+    userEvent.click(submitButton);
+
+    expect(history.location.pathname).toEqual('/search/vuejs');
+  });
+
+  test('input value changes to default subreddit when search link in header is clicked', () => {
+    setup('/search/reactjs');
+    const searchInput = screen.getByRole('textbox');
+    const header = screen.getByRole('banner');
+    const searchLink = within(header).getByRole('link', { name: /Search/ });
+
+    userEvent.click(searchLink);
+
+    expect(searchInput.value).toBe(defaultSubreddit);
+  });
 });
 
-test('renders error message', async () => {
-  setup('/search/failing-request');
-  expect(await screen.findByText(/something went wrong/i)).toBeInTheDocument();
-});
+describe('heatmap', () => {
+  test('loads and renders top posts for subreddit in URL', async () => {
+    setup('/search/reactjs');
 
-test('submitting the form updates the URL', () => {
-  const { history } = setup('/search/reactjs');
+    screen.getByText('loading-spinner.svg');
 
-  const searchInput = screen.getByLabelText('r /');
-  expect(searchInput.value).toBe('reactjs');
+    expect(await screen.findByTestId('heatmap')).toBeInTheDocument();
+    expect(screen.queryByText('loading-spinner.svg')).not.toBeInTheDocument();
 
-  userEvent.clear(searchInput);
-  userEvent.type(searchInput, 'vuejs');
-  expect(searchInput.value).toBe('vuejs');
+    const heatmap = screen.getByTestId('heatmap');
+    const cells = await within(heatmap).findAllByRole('button');
+    expect(cells.length).toEqual(7 * 24);
 
-  const submitButton = screen.getByRole('button', { name: 'Search' });
-  userEvent.click(submitButton);
+    const numberOfPostsPerCell = cells.map((cell) => cell.innerHTML);
+    expect(numberOfPostsPerCell).toMatchSnapshot();
+  });
 
-  expect(history.location.pathname).toEqual('/search/vuejs');
-});
+  test('cell highlights on click', async () => {
+    setup('/search/reactjs');
+    const heatmap = await screen.findByTestId('heatmap');
+    const cells = await within(heatmap).findAllByRole('button');
 
-test('input value changes to default subreddit when search link in header is clicked', () => {
-  setup('/search/reactjs');
-  const searchInput = screen.getByRole('textbox');
-  const header = screen.getByRole('banner');
-  const searchLink = within(header).getByRole('link', { name: /Search/ });
+    const cellToClick = cells[1];
+    expect(cellToClick).toHaveStyle('border: none');
 
-  userEvent.click(searchLink);
+    userEvent.click(cellToClick);
+    expect(cellToClick).toHaveStyle('border: 1px solid #1e2537');
+  });
 
-  expect(searchInput.value).toBe(defaultSubreddit);
+  test('renders error message', async () => {
+    setup('/search/failing-request');
+
+    expect(await screen.findByText(/something went wrong/i)).toBeInTheDocument();
+    expect(screen.queryByText('loading-spinner.svg')).not.toBeInTheDocument();
+  });
 });
